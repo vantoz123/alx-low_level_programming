b6ccfb07b74a71186d7036d97ffaa57f47c479fb
vantoz123 <kosgeievans66@gmail.com>
2023-02-21 05:41:50 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+/**
+ * print_alphabet_10x - Prints the alphabet in lowercase 10 times, each followed by a new line.
+ */
+void print_alphabet_10x(void)
+{
+    int i, j;
+    char c;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+        {
+            putchar(c);
+        }
+        putchar('\n');
+    }
+}
+
