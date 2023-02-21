c22862256ff5866ae190729c90e75ee415a71418
vantoz123 <kosgeievans66@gmail.com>
2023-02-21 08:44:06 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+/**main print _putchar
+ * is witten by vantoz
+ * return 0
  */
 int main(void)
 {
-	char *sh = "Holberton";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
+
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char)
+{
+        return (write(1, &c, 1));
+}
+
