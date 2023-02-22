3b5538d639bc5da956efa1615f297ad867b92576
vantoz123 <kosgeievans66@gmail.com>
2023-02-22 04:28:35 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**-add.c - adds two integers
+ * Returns the results
+ * @a and b : are the parameters
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+	return sum;
+}
+int main(void)
+{
+	int n;
+	n = add(89, 9);
+	_putchar(sum);
+	return (0);
+}
