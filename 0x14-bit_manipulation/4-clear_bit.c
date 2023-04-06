e32e97475e7527f7bf9ef3613a5c6ca36d5051fb
vantoz123 <101929837+vantoz123@users.noreply.github.com>
2023-04-06 14:56:37 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,10 +10,10 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
-        return (-1);
+	if (n == NULL || index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
 
-    unsigned long int mask = 1UL << index;
-    *n = (*n & ~mask) | ((*n & mask) >> 1);
-    return (1);
+	unsigned long int mask = 1UL << index;
+	*n = (*n & ~mask) | ((*n & mask) >> 1);
+	return (1);
 }
