7faa9e0f0af5e0b02102b24661afab9f8fe42058
vantoz123 <101929837+vantoz123@users.noreply.github.com>
2023-04-06 14:42:25 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,6 +10,9 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+    if (index > 63)
+		return (-1);
+    
     /* Shift n right by index bits, then use bitwise AND to get the value at the given index */
     return (n >> index) & 1;
 }
